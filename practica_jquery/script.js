874d29c7d2b92a2b6686cf8333f1ad806355df2a
Xvegetta <72033570+Xvegetta@users.noreply.github.com>
2021-10-08 15:58:38 -0500

    practica del uso en jQuery

---
+++ practica_jquery/script.js
@@ -0,0 +1,33 @@
+$( document ).ready(function() {
+    cargarValores();
+    $('#example').DataTable();
+});
+
+var total = 0;
+$(".botones").click(function()
+{   
+    var valor = $(this).attr("valor");
+    //console.log(valor);
+    if(valor == 1){
+        $(this).css("background-color", "white");            
+        total++;
+    }else{
+        $(this).css("background-color", "red");
+        alert("Perdiste, Puntos obtenidos: "+total);  
+        location.reload();          
+    }
+
+});
+
+function valorAleatorio(min = 0, max = 1){
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function cargarValores(){
+    $(".verificar").each(function(){
+        $(this).attr("valor",valorAleatorio);            
+    });
+}
+
+
+
